643fcb1cb173543aa083bf3bcff372acdfbca803
Sanjin Dumancic <sanjin.dumancic@yahoo.com>
2021-12-11 18:53:59 +0100

    improvements

--- src/app/tree-drag-drop/tree-drag-drop-home/tree-drag-drop-home.component.ts
+++ src/app/tree-drag-drop/tree-drag-drop-home/tree-drag-drop-home.component.ts
@@ -1,10 +1,12 @@
 import {
   AfterViewInit,
   Component,
+  ElementRef,
   OnInit,
   ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
+import { MatInput } from '@angular/material/input';
 import {
   TreeComponent,
   TreeModel,
@@ -42,10 +44,15 @@ function copyObjectAndUpdateId(aObject) {
 })
 export class TreeDragDropHomeComponent implements OnInit, AfterViewInit {
   @ViewChild('targetTree') targetTree: TreeComponent;
+  @ViewChild('filter', { static: true }) filterElementRef: ElementRef;
+
   generatedMappingJson: string;
   nodeTypes = [
     { value: 'object', viewValue: 'Object' },
     { value: 'technical-attributes', viewValue: 'Technical Attributes' },
+    { value: 'string', viewValue: 'String' },
+    { value: 'number', viewValue: 'Number' },
+    { value: 'date', viewValue: 'Date' },
   ];
   nodes2: any[];
   nodes = [
@@ -53,18 +60,21 @@ export class TreeDragDropHomeComponent implements OnInit, AfterViewInit {
       id: 'orders.id',
       key: 'orders.id',
       name: 'id',
+      type: 'number',
       _dragAction: 'clone',
     },
     {
       id: 'orders.orderDate',
       key: 'orders.orderDate',
       name: 'orderDate',
+      type: 'date',
       _dragAction: 'clone',
     },
     {
       id: 'orders.processDate',
       key: 'orders.processDate',
       name: 'processDate',
+      type: 'date',
       _dragAction: 'clone',
     },
     {
@@ -72,17 +82,20 @@ export class TreeDragDropHomeComponent implements OnInit, AfterViewInit {
       key: 'orders.orderStatus',
       name: 'orderStatus',
       _dragAction: 'clone',
+      type: 'object',
       children: [
         {
           id: 'orders.orderStatus.code',
           key: 'orders.orderStatus.code',
           name: 'code',
+          type: 'string',
           _dragAction: 'clone',
         },
         {
           id: 'orders.orderStatus.value',
           key: 'orders.orderStatus.value',
           name: 'value',
+          type: 'string',
           _dragAction: 'clone',
         },
       ],
@@ -91,42 +104,49 @@ export class TreeDragDropHomeComponent implements OnInit, AfterViewInit {
       id: 'orders.customer',
       key: 'orders.customer',
       name: 'customer',
+      type: 'object',
       _dragAction: 'clone',
       children: [
         {
           id: 'orders.customer.id',
           key: 'orders.customer.id',
           name: 'id',
+          type: 'number',
           _dragAction: 'clone',
         },
         {
           id: 'orders.customer.name',
           key: 'orders.customer.name',
           name: 'name',
+          type: 'string',
           _dragAction: 'clone',
         },
         {
           id: 'orders.customer.address',
           key: 'orders.customer.address',
           name: 'address',
+          type: 'string',
           _dragAction: 'clone',
         },
         {
           id: 'orders.customer.city',
           key: 'orders.customer.city',
           name: 'city',
+          type: 'string',
           _dragAction: 'clone',
         },
         {
           id: 'orders.customer.phones',
           key: 'orders.customer.phones',
           name: 'phones',
+          type: 'array',
           _dragAction: 'clone',
           children: [
             {
               id: 'orders.customer.phones.value',
               key: 'orders.customer.phones.value',
               name: 'value',
+              type: 'string',
               _dragAction: 'clone',
             },
           ],
@@ -137,24 +157,28 @@ export class TreeDragDropHomeComponent implements OnInit, AfterViewInit {
       id: 'orders.items',
       key: 'orders.items',
       name: 'items',
+      type: 'object',
       _dragAction: 'clone',
       children: [
         {
           id: 'orders.items.id',
           key: 'orders.items.id',
           name: 'id',
+          type: 'number',
           _dragAction: 'clone',
         },
         {
           id: 'orders.items.price',
           key: 'orders.items.price',
           name: 'price',
+          type: 'number',
           _dragAction: 'clone',
         },
         {
           id: 'orders.items.name',
           key: 'orders.items.name',
           name: 'name',
+          type: 'string',
           _dragAction: 'clone',
         },
       ],
@@ -163,18 +187,13 @@ export class TreeDragDropHomeComponent implements OnInit, AfterViewInit {
       id: 'orders.orderTotal',
       key: 'orders.orderTotal',
       name: 'orderTotal',
+      type: 'number',
       _dragAction: 'clone',
     },
   ];
   optionsSource = {
     allowDrag: true,
     allowDrop: true,
-
-    getNodeClone: (node) => ({
-      ...node.data,
-      id: uuidv4(),
-      name: `copy of ${node.data.name}`,
-    }),
   };
   optionsTarget = {
     allowDrag: true,
@@ -228,6 +247,7 @@ export class TreeDragDropHomeComponent implements OnInit, AfterViewInit {
       this.nodes2.push({
         id: uuidv4(),
         name: 'a new child',
+        type: 'string',
         _dragAction: 'move',
       });
       this.targetTree.treeModel.update();
@@ -236,13 +256,43 @@ export class TreeDragDropHomeComponent implements OnInit, AfterViewInit {
     if (activeNode.data.children === undefined) {
       activeNode.data.children = new Array();
     }
+    console.log(activeNode.data.children);
     activeNode.data.children.push({
       id: uuidv4(),
       name: 'a new child',
       _dragAction: 'move',
+      type: 'string',
+      hasChildren: false,
+    });
+    console.log(activeNode.data.children);
+
+    this.targetTree.treeModel.update();
+    if (activeNode.isCollapsed) {
+      this.targetTree.treeModel.getActiveNode().toggleExpanded();
+    }
+
+    this.generateMapping();
+  }
+
+  addChildNode(parent: TreeNode) {
+    this.targetTree.treeModel.setFocus(true);
+    if (parent.data.children === undefined) {
+      parent.data.children = new Array();
+    }
+    parent.data.children.push({
+      id: uuidv4(),
+      name: 'a new child',
+      type: 'string',
+      _dragAction: 'move',
       hasChildren: false,
     });
     this.targetTree.treeModel.update();
+    if (parent.isCollapsed) {
+      console.log('parent is collapsed');
+      parent.toggleExpanded();
+      this.targetTree.treeModel.getNodeById(parent.data.id).toggleExpanded();
+    }
+
     this.generateMapping();
   }
 
@@ -303,4 +353,17 @@ export class TreeDragDropHomeComponent implements OnInit, AfterViewInit {
       results.push(name + ' : ' + key);
     }
   }
+
+  emptyTree() {
+    this.nodes2 = [];
+    this.targetTree.treeModel.update();
+    this.generateMapping();
+  }
+
+  clearFilter() {
+    this.targetTree.treeModel.clearFilter();
+    this.filterElementRef.nativeElement.value = '';
+  }
+
+  generateJson() {}
 }
--- src/app/tree-drag-drop/tree-drag-drop.module.ts
+++ src/app/tree-drag-drop/tree-drag-drop.module.ts
@@ -4,6 +4,7 @@ import { TreeDragDropHomeComponent } from './tree-drag-drop-home/tree-drag-drop-
 import { TreeModule } from '@circlon/angular-tree-component';
 import { TreeDragDropRoutingModule } from './tree-drag-drop-routing.module';
 import { MaterialModule } from '../shared/modules/material/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TreeDragDropHomeComponent],
@@ -12,6 +13,7 @@ import { MaterialModule } from '../shared/modules/material/material.module';
     TreeModule,
     TreeDragDropRoutingModule,
     MaterialModule,
+    FormsModule,
   ],
 })
 export class TreeDragDropModule {}
